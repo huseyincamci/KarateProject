1ebf6e1d41e1eddd1d74b9eccb050dbb7d3c9d63
Huseyin <hcamci.ce@gmail.com>
2025-01-05 19:21:39 +0300

    Cucumber Reporter

--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -3,7 +3,16 @@ package conduitApp;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
 class ConduitTest {
@@ -11,9 +20,19 @@ class ConduitTest {
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:conduitApp")
-                //.outputCucumberJson(true)
+                .outputCucumberJson(true)
                 .parallel(5);
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+                generateReport(results.getReportDir());
+        assertTrue(results.getFailCount() == 0, results.getErrorMessages());
+    }
+
+     public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "conduitApp");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 
 }
